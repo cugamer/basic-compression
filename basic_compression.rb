4d94c854d4e20e1723f3b965afe1b67cccb3089d
Jonathon Nordquist <jonathon.nordquist@gmail.com>
2020-10-16 17:53:09 -0500

    Refactor

--- basic_compression.rb
+++ basic_compression.rb
@@ -1,19 +1,18 @@
-require 'pry'
+# frozen_string_literal: true
 
 class BasicCompression
   def build_compressed_string(input_str)
     output = ''
     pointer = 0
-    
-    while(pointer < input_str.length) do
-      if(input_str[pointer] == input_str[pointer + 1])
-        compressed_sequence = read_string_sequence(input_str[pointer..input_str.length])
-        output += compressed_sequence[0]
-        pointer += compressed_sequence[1]
-      else
-        output += input_str[pointer]
-        pointer += 1
-      end
+
+    while pointer < input_str.length
+      result = if input_str[pointer] == input_str[pointer + 1]
+                 read_string_sequence(input_str[pointer..input_str.length])
+               else
+                 [input_str[pointer], 1]
+               end
+      output += result[0]
+      pointer += result[1]
     end
     output
   end
@@ -21,18 +20,14 @@ class BasicCompression
   def read_string_sequence(repeating_string)
     count = 1
 
-    while(count < repeating_string.length && repeating_string[count - 1] == repeating_string[count]) do
-      count += 1
-    end
+    count += 1 while repeating_string[count - 1] == repeating_string[count]
     ["#{repeating_string[0]}#{count}", count]
   end
 end
 
-
-# Simple test sequences
+# Simple test sequences, uncomment to run
 basic = BasicCompression.new
-p basic.build_compressed_string('abcdefg') == 'abcdefg';
-p basic.build_compressed_string('abbcccdddd') == 'ab2c3d4';
-p basic.build_compressed_string('abcaaabbb') == 'abca3b3';
-p basic.build_compressed_string('aaabaaaaccaaaaba') == 'a3ba4c2a4ba';
-
+p basic.build_compressed_string('abcdefg') == 'abcdefg'
+p basic.build_compressed_string('abbcccdddd') == 'ab2c3d4'
+p basic.build_compressed_string('abcaaabbb') == 'abca3b3'
+p basic.build_compressed_string('aaabaaaaccaaaaba') == 'a3ba4c2a4ba'
